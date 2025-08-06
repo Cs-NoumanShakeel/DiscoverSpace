c97ed438d1ed69b329062cdb6a9c087a2093cb02
Nouman Shakeel <noumannorm648@gmail.com>
2025-08-06 23:33:14 +0500

    still modifying css

--- frontend/src/pages/mission/historical.jsx
+++ frontend/src/pages/mission/historical.jsx
@@ -194,7 +194,7 @@ export default function Historical() {
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       className="mission-video"
-      style={{ width: '100%', maxWidth: '800px', height: '450px' }}
+     // style={{ width: '100%', maxWidth: '800px', height: '450px' }}
     ></iframe>
   </div>
 )}
